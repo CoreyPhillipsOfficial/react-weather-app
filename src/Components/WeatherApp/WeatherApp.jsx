3885eec3e703096ec46af1678abb27086661c44f
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2024-01-29 16:03:14 -0500

    working with weather api

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -11,12 +11,56 @@ import wind_icon from '../Assets/wind.png';
 import humidity_icon from '../Assets/humidity.png';
 
 export const WeatherApp = () => {
+
+    let api_key = 'a9b44091ebf86e0565ff1705218036ba'
+
+    const search = async () => {
+        const element = document.getElementsByClassName('cityInput')
+        if (element[0].value === '') {
+            return 0;
+        }
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=imperial&appid=${api_key}`;
+
+        let response = await fetch(url);
+        let data = await response.json();
+        const humidity = document.getElementsByClassName('humidity-percent');
+        const wind = document.getElementsByClassName('wind-rate');
+        const temperature = document.getElementsByClassName('weather-temp');
+        const location = document.getElementsByClassName('weather-location');
+
+        humidity[0].innerHTML = data.main.humidity;
+        wind[0].innerHTML = data.wind.speed;
+        temperature[0].innerHTML = data.main.temp;
+        location[0].innerHTML = data.name;
+    }
+
     return (
         <div className='container'>
             <div className="top-bar">
                 <input type="text" className="cityInput" placeholder='Search a city' />
-                <div className="search-icon">
+                <div className="search-icon" onClick={() => { search() }}>
                     <img src={search_icon} alt="" />
+                </div>
+            </div>
+            <div className="weather-image">
+                <img src={cloud_icon} alt="" />
+            </div>
+            <div className="weather-temp">24˚c</div>
+            <div className="weather-location">London</div>
+            <div className="data-container">
+                <div className="element">
+                    <img src={humidity_icon} alt="" className='icon' />
+                    <div className="data">
+                        <div className="humidity-percent">64%</div>
+                        <div className="text">Humidity</div>
+                    </div>
+                </div>
+                <div className="element">
+                    <img src={wind_icon} alt="" className='icon' />
+                    <div className="data">
+                        <div className="wind-rate">18 m/hr</div>
+                        <div className="text">Wind Speed</div>
+                    </div>
                 </div>
             </div>
         </div>

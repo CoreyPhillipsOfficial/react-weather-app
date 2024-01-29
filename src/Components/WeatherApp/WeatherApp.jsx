791ef44e0ffe70e71f4cfdf1a53f99ebed149c71
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2024-01-29 14:49:24 -0500

    imported search icons

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import './WeatherApp'
+
+import search_icon from '../Assets/search.png';
+import clear_icon from '../Assets/clear.png';
+import cloud_icon from '../Assets/cloud.png';
+import drizzle_icon from '../Assets/drizzle.png';
+import rain_icon from '../Assets/rain.png';
+import snow_icon from '../Assets/snow.png';
+import wind_icon from '../Assets/wind.png';
+import humidity_icon from '../Assets/humidity.png';
 
 export const WeatherApp = () => {
     return (
-        <div>
+        <div className='container'>
+            <div className="top-bar">
+                <input type="text" className="cityInput" placeholder='Search a city' />
+                <div className="search-icon">
 
+                </div>
+            </div>
         </div>
     )
 }

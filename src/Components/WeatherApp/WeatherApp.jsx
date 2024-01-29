d82b246b63e5e73a9593bf94ca29cb9cb7dfe0f6
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2024-01-29 18:19:48 -0500

    creating a unit toggle ability

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './WeatherApp.css'
 
 import search_icon from '../Assets/search.png';
@@ -13,39 +13,87 @@ import humidity_icon from '../Assets/humidity.png';
 export const WeatherApp = () => {
 
     let api_key = 'a9b44091ebf86e0565ff1705218036ba'
+    const [wicon, setWicon] = useState(cloud_icon);
+    const [unit, setUnit] = useState('imperial'); // Default to imperial units
+    const [temperature, setTemperature] = useState(null);
 
     const search = async () => {
         const element = document.getElementsByClassName('cityInput')
         if (element[0].value === '') {
             return 0;
         }
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=imperial&appid=${api_key}`;
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=${unit}&appid=${api_key}`;
 
         let response = await fetch(url);
         let data = await response.json();
         const humidity = document.getElementsByClassName('humidity-percent');
         const wind = document.getElementsByClassName('wind-rate');
-        const temperature = document.getElementsByClassName('weather-temp');
+        const temperatureElement = document.getElementsByClassName('weather-temp');
         const location = document.getElementsByClassName('weather-location');
 
-        humidity[0].innerHTML = data.main.humidity;
-        wind[0].innerHTML = data.wind.speed;
-        temperature[0].innerHTML = data.main.temp;
+        humidity[0].innerHTML = data.main.humidity + ' %';
+        wind[0].innerHTML = Math.floor(data.wind.speed) + ' m/hr';
+
+        // Set the temperature in the state
+        setTemperature(Math.floor(data.main.temp));
+
+        // Set the temperature unit in the state
+        setUnit(unit);
+
+        temperatureElement[0].innerHTML = Math.floor(data.main.temp) + '˚F';
         location[0].innerHTML = data.name;
+
+        if (data.weather[0].icon === '01d' || data.weather[0].icon === '01n') {
+            setWicon(clear_icon);
+        }
+        else if (data.weather[0].icon === '02d' || data.weather[0].icon === '02n') {
+            setWicon(cloud_icon);
+        }
+        else if (data.weather[0].icon === '03d' || data.weather[0].icon === '03n') {
+            setWicon(drizzle_icon);
+        }
+        else if (data.weather[0].icon === '04d' || data.weather[0].icon === '04n') {
+            setWicon(drizzle_icon);
+        }
+        else if (data.weather[0].icon === '09d' || data.weather[0].icon === '09n') {
+            setWicon(rain_icon);
+        }
+        else if (data.weather[0].icon === '10d' || data.weather[0].icon === '10n') {
+            setWicon(cloud_icon);
+        }
+        else if (data.weather[0].icon === '13d' || data.weather[0].icon === '13n') {
+            setWicon(snow_icon);
+        }
+        else {
+            setWicon(clear_icon);
+        }
     }
+
+    const toggleUnit = () => {
+        // Toggle between 'imperial' and 'metric' units
+        setUnit(unit === 'imperial' ? 'metric' : 'imperial');
+        // Call the search again with the updated unit
+        search();
+    };
 
     return (
         <div className='container'>
-            <div className="top-bar">
-                <input type="text" className="cityInput" placeholder='Search a city' />
-                <div className="search-icon" onClick={() => { search() }}>
-                    <img src={search_icon} alt="" />
+            <div className='top-bar'>
+                <input type='text' className='cityInput' placeholder='Search a city' />
+                <div className='search-icon' onClick={() => search()}>
+                    <img src={search_icon} alt='' />
                 </div>
+                <button onClick={toggleUnit}>
+                    Toggle Unit ({unit === 'imperial' ? '˚F' : '˚C'})
+                </button>
             </div>
             <div className="weather-image">
-                <img src={cloud_icon} alt="" />
+                <img src={wicon} alt="" />
             </div>
-            <div className="weather-temp">24˚c</div>
+            <div className="weather-temp">
+                {temperature !== null && `${temperature}˚${unit === 'imperial' ? 'F' : 'C'}`}
+            </div>
             <div className="weather-location">London</div>
             <div className="data-container">
                 <div className="element">
